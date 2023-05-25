cc45bfe9113fa9cdecc65a82ca6e6fe867263b96
d-efimov <heyhellomyfrends@gmail.com>
2023-05-25 22:23:21 +0300

    Add Vacatation, Sotrydnik

---
+++ Models/Sotrydnik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SAP_10.Models
+{
+    public class Sotrydnik
+    {
+        public int SotrydnikId { get; set; }
+
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string patronymic { get; set; }
+        public string job_title { get; set; }
+        public string subdivision { get; set; }
+        public DateTime employment_date { get; set; }
+    }
+}
---
+++ Models/Vacation.cs
@@ -0,0 +1,11 @@
+﻿namespace SAP_10.Models
+{
+    public class Vacation
+    {
+        public int VacationId { get; set; }
+        public string vacation_type { get; set; }
+        public string vacation_pay { get; set; }
+        public string holiday_benefits { get; set; }
+
+    }
+}
