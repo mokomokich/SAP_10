8d36698719b42af107f22862349b70a2ecbf018d
d-efimov <heyhellomyfrends@gmail.com>
2023-05-25 22:49:13 +0300

    Add controllers Sotrydnik,Vacations

---
+++ Controllers/SotrydniksController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SAP_10.Data;
+using SAP_10.Models;
+
+namespace SAP_10.Controllers
+{
+    public class SotrydniksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SotrydniksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sotrydniks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sotrydnik.ToListAsync());
+        }
+
+        // GET: Sotrydniks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sotrydnik = await _context.Sotrydnik
+                .FirstOrDefaultAsync(m => m.SotrydnikId == id);
+            if (sotrydnik == null)
+            {
+                return NotFound();
+            }
+
+            return View(sotrydnik);
+        }
+
+        // GET: Sotrydniks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sotrydniks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SotrydnikId,surname,name,patronymic,job_title,subdivision,employment_date")] Sotrydnik sotrydnik)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sotrydnik);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sotrydnik);
+        }
+
+        // GET: Sotrydniks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sotrydnik = await _context.Sotrydnik.FindAsync(id);
+            if (sotrydnik == null)
+            {
+                return NotFound();
+            }
+            return View(sotrydnik);
+        }
+
+        // POST: Sotrydniks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SotrydnikId,surname,name,patronymic,job_title,subdivision,employment_date")] Sotrydnik sotrydnik)
+        {
+            if (id != sotrydnik.SotrydnikId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sotrydnik);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SotrydnikExists(sotrydnik.SotrydnikId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sotrydnik);
+        }
+
+        // GET: Sotrydniks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sotrydnik = await _context.Sotrydnik
+                .FirstOrDefaultAsync(m => m.SotrydnikId == id);
+            if (sotrydnik == null)
+            {
+                return NotFound();
+            }
+
+            return View(sotrydnik);
+        }
+
+        // POST: Sotrydniks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sotrydnik = await _context.Sotrydnik.FindAsync(id);
+            _context.Sotrydnik.Remove(sotrydnik);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SotrydnikExists(int id)
+        {
+            return _context.Sotrydnik.Any(e => e.SotrydnikId == id);
+        }
+    }
+}
---
+++ Controllers/VacationsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SAP_10.Data;
+using SAP_10.Models;
+
+namespace SAP_10.Controllers
+{
+    public class VacationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VacationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vacations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vacation.ToListAsync());
+        }
+
+        // GET: Vacations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacation = await _context.Vacation
+                .FirstOrDefaultAsync(m => m.VacationId == id);
+            if (vacation == null)
+            {
+                return NotFound();
+            }
+
+            return View(vacation);
+        }
+
+        // GET: Vacations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vacations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VacationId,vacation_type,vacation_pay,holiday_benefits")] Vacation vacation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vacation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vacation);
+        }
+
+        // GET: Vacations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacation = await _context.Vacation.FindAsync(id);
+            if (vacation == null)
+            {
+                return NotFound();
+            }
+            return View(vacation);
+        }
+
+        // POST: Vacations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VacationId,vacation_type,vacation_pay,holiday_benefits")] Vacation vacation)
+        {
+            if (id != vacation.VacationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vacation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VacationExists(vacation.VacationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vacation);
+        }
+
+        // GET: Vacations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacation = await _context.Vacation
+                .FirstOrDefaultAsync(m => m.VacationId == id);
+            if (vacation == null)
+            {
+                return NotFound();
+            }
+
+            return View(vacation);
+        }
+
+        // POST: Vacations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vacation = await _context.Vacation.FindAsync(id);
+            _context.Vacation.Remove(vacation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VacationExists(int id)
+        {
+            return _context.Vacation.Any(e => e.VacationId == id);
+        }
+    }
+}
